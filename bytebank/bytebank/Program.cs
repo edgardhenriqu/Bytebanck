7f4c02b1d7f31425c3f660ee7b9e724e3ef13666
Edgard <edigarhenriqu@hmail.com>
2023-05-20 18:46:17 -0300

    Conta corrente

--- bytebank/bytebank/Program.cs
+++ bytebank/bytebank/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using bytebank;
+
+ContaCorrente contaDoAndre = new ContaCorrente();
+contaDoAndre.titular = "André Silva";
+contaDoAndre.numero_agencia = 15;
+contaDoAndre.conta = "1010-X";
+contaDoAndre.saldo = 100;
+
+Console.WriteLine("saldo da conta do André = " + contaDoAndre.saldo);
