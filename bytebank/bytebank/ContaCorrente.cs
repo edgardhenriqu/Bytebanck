c2a94a3c30090e8909b0200a66411682816a66cc
Edgard <edigarhenriqu@hmail.com>
2023-05-22 00:04:09 -0300

    Sacar

--- bytebank/bytebank/ContaCorrente.cs
+++ bytebank/bytebank/ContaCorrente.cs
@@ -12,5 +12,23 @@ namespace bytebank
         public string conta;
         public string titular;
         public double saldo;
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
--- bytebank/bytebank/Program.cs
+++ bytebank/bytebank/Program.cs
@@ -7,3 +7,17 @@ contaDoAndre.conta = "1010-X";
 contaDoAndre.saldo = 100;
 
 Console.WriteLine("saldo da conta do André = " + contaDoAndre.saldo);
+
+contaDoAndre.Depositar(100);
+
+Console.WriteLine("Saldo da conta do André pós-deposito =" + contaDoAndre.saldo);
+
+if (contaDoAndre.Sacar(300) ==true)
+{
+    Console.WriteLine("Saldo da conta do André pós-saque =" + contaDoAndre.saldo);
+}
+else
+{
+    Console.WriteLine("Saldo insuficiente para saque");
+}
+
